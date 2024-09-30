f0d24bbfb2de847abdc0a667b9f9f1843398329a
Fernando <fer.lozano10@hotmail.com>
2024-09-30 13:56:35 +0200

    first commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -241,7 +241,7 @@ ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = [("""Fernando Lozano""", "fer.lozano10@hotmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
-MANAGERS = [("""Mint""", "mint@mint.com")]
+MANAGERS = [("""your name """, "your@email.com ")]
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
 # Force the `admin` sign in process to go through the `django-allauth` workflow
 DJANGO_ADMIN_FORCE_ALLAUTH = env.bool("DJANGO_ADMIN_FORCE_ALLAUTH", default=False)
